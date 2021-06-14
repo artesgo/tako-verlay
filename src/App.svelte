bac634932f25ac11259ce913315342ee58304954
artesgo <artesgo@gmail.com>
2021-06-14 08:35:13 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import Bottom from './lib/Bottom.svelte';
+import Left from './lib/Left.svelte';
+import Right from './lib/Right.svelte';
+import Top from './lib/Top.svelte';
+</script>
+
+<main>
+  <div class="top flex around">
+    <Top></Top>
+  </div>
+  <div class="middle flex between">
+    <div class="left">
+      <Left></Left>
+    </div>
+    <div class="right">
+      <Right></Right>
+    </div>
+  </div>
+  <div class="bottom flex around">
+    <Bottom></Bottom>
+  </div>
+</main>
+
+<style global>
+  body {
+    background: green;
+    padding: 0;
+    margin: 0;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100vh;
+  }
+  .flex {
+    display: flex;
+  }
+  .between {
+    justify-content: space-between; 
+  }
+  .around {
+    justify-content: space-around; 
+  }
+</style>
---
+++ src/lib/Top.svelte
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <path d="m 0 0 l 10 10 l 80 0 l 10 -10 z " 
+    fill="#ffff00" stroke="#000000" stroke-width="1" />
+</svg>
+
+<style>
+    svg {
+        width: 100px;
+    }
+</style>
